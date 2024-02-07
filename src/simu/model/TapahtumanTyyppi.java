1370c4d251f9bd078b43720a782cdb3b5aab0be8
mirohi <m_hintikka@hotmail.com>
2024-02-07 15:17:14 +0200

    Simulator new palvelupiste and updated names

--- src/simu/model/TapahtumanTyyppi.java
+++ src/simu/model/TapahtumanTyyppi.java
@@ -5,6 +5,6 @@ import src.simu.framework.ITapahtumanTyyppi;
 // TODO:
 // Tapahtumien tyypit määritellään simulointimallin vaatimusten perusteella
 public enum TapahtumanTyyppi implements ITapahtumanTyyppi{
-	ARR1, Infotiski, UudenTilinAvaus, Talletus, SijoitusPalvelut;
+	ARR1, INFOTISKI, UUDEN_TILIN_AVAUS, TALLETUS, SIJOITUS_PALVELUT;
 
 }
